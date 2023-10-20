a141e17eda807be417826119048125ead26bb6aa
Tron Schell <tys6181@rit.edu>
2023-10-19 20:10:22 -0400

    added firebase support

--- crunchbase-alt/src/App.js
+++ crunchbase-alt/src/App.js
@@ -1,6 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9OmsFbFTBguC3yGUNYMipgRK9mgcge_g",
+  authDomain: "evocative-hour-402623.firebaseapp.com",
+  projectId: "evocative-hour-402623",
+  storageBucket: "evocative-hour-402623.appspot.com",
+  messagingSenderId: "837082112135",
+  appId: "1:837082112135:web:8f029b27e6ae6ad3229822"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 function App() {
   return (
     <div className="App">
